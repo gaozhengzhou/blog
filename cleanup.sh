f2742c0df2564daa633f8bec5d119ea927867a09
gzz <kuqq2005@qq.com>
2020-02-02 11:29:21 +0800

    日积月累

---
+++ cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo docker rm -f $(sudo docker ps -qa)
+sudo docker volume rm $(sudo docker volume ls -q)
+cleanupdirs="/var/lib/etcd /etc/kubernetes /etc/cni /opt/cni /var/lib/cni /var/run/calico /opt/rke"
+for dir in $cleanupdirs; do
+  echo "Removing $dir"
+  rm -rf $dir
+done
